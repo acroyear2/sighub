7a4b125f8e0bb5808225c4a5a0d6fedc74287e23
tetsuo <ogunduz@gmail.com>
2015-05-19 00:18:16 +0300

    remove client

--- bin/cmd.js
+++ bin/cmd.js
@@ -12,42 +12,17 @@ var argv = minimist(process.argv.slice(2), {
   }
 });
 
-var cmd = argv._[0];
+var max = Number(argv['max-broadcasts']) || 0;
+var server = require('../')({maxBroadcasts: max});
 
-if (cmd === 'listen') {
-  var max = Number(argv['max-broadcasts']) || 0;
-  var server = require('../server')({maxBroadcasts: max});
+server.on('subscribe', function (channel) {
+  console.log('subscribe: %s', channel);
+});
 
-  server.on('subscribe', function (channel) {
-    console.log('subscribe: %s', channel);
-  });
+server.on('broadcast', function (channel, message) {
+  console.log('broadcast: %s (%d)', channel, message.length);
+});
 
-  server.on('broadcast', function (channel, message) {
-    console.log('broadcast: %s (%d)', channel, message.length);
-  });
-
-  server.listen(argv.port, function () {
-    console.log('signalhub listening on port %d', server.address().port);
-  });
-  return;
-}
-
-if (cmd === 'subscribe') {
-  if (argv.length < 3)
-    return console.error('Usage: signalhub subscribe [app] [channel]');
-  var client = require('../')(argv._[1], argv.host || 'localhost');
-  client.subscribe(argv._[2]).on('data', function (data) {
-    console.log(data);
-  });
-  return;
-}
-
-if (cmd === 'broadcast') {
-  if (argv.length < 4)
-    return console.error('Usage: signalhub broadcast [app] [channel] [json-message]');
-  var client = require('../')(argv._[1], argv.host || 'localhost');
-  client.broadcast(argv._[2], JSON.parse(argv._[3]));
-  return;
-}
-
-console.error('Usage: signalhub listen|subscribe|broadcast');
+server.listen(argv.port, function () {
+  console.log('listening on: ' + server.address().port);
+});
--- index.js
+++ index.js
@@ -1,66 +1,74 @@
-var ess = require('event-source-stream');
-var nets = require('nets');
+var http = require('http');
+var cors = require('corsify');
+var collect = require('stream-collector');
 var pump = require('pump');
-var through = require('through2');
+var iterate = require('random-iterate');
+var limiter = require('size-limit-stream');
+var eos = require('end-of-stream');
 
-var noop = function () {};
+module.exports = function (opts) {
+  var channels = {};
+  var maxBroadcasts = (opts && opts.maxBroadcasts) || Infinity;
 
-module.exports = function (app, urls) {
-  if (!app) throw new Error('app name required');
-  if (!urls || !urls.length) throw new Error('signalhub url(s) required');
-
-  var self = {};
-  if (!Array.isArray(urls)) urls = [urls];
-
-  urls = urls.map(function (url) {
-    return url.indexOf('://') === -1 ? 'http://' + url : url;
-  });
-
-  self.subscribe = function (channel) {
-    var all = through.obj();
-    var streams = urls.map(function (url) {
-      return ess(url + '/v1/' + app + '/' + channel, {json: true});
-    });
-
-    if (streams.length === 1) return streams[0];
-
-    var all = through.obj();
-
-    all.setMaxListeners(0);
-    streams.forEach(function (stream) {
-      pump(stream, all);
-    });
-
-    return all;
+  var get = function (channel) {
+    if (channels[channel]) return channels[channel];
+    channels[channel] = { name: channel, subscribers: [] };
+    return channels[channel];
   }
 
-  var broadcast = function (url, channel, message, cb) {
-    nets({
-      method: 'POST',
-      json: message,
-      url: url + '/v1/' + app + '/' + channel
-    }, function (err, res) {
-      if (err) return cb(err);
-      if (res.statusCode !== 200) return cb(new Error('Bad status: ' + res.statusCode));
-      cb();
-    });
-  }
+  var server = http.createServer(cors(function (req, res) {
+    if (req.url === '/') {
+      res.end(JSON.stringify({name: 'signalhub', version: require('./package').version}, null, 2) + '\n');
+      return;
+    }
 
-  self.broadcast = function (channel, message, cb) {
-    if (!cb) cb = noop;
+    if (req.url.slice(0, 4) !== '/v1/') {
+      res.statusCode = 404;
+      res.end();
+      return;
+    }
 
-    var pending = urls.length;
-    var errors = 0;
+    var name = req.url.slice(4).split('?')[0];
 
-    urls.forEach(function (url) {
-      broadcast(url, channel, message, function (err) {
-        if (err) errors++;
-        if (--pending) return;
-        if (errors === urls.length) return cb(err);
-        cb();
+    if (req.method === 'POST') {
+      collect(pump(req, limiter(64 * 1024)), function (err, data) {
+        if (err) return res.end();
+        if (!channels[name]) return res.end();
+        var channel = get(name);
+
+        server.emit('publish', channel.name, data);
+        data = Buffer.concat(data).toString();
+
+        var ite = iterate(channel.subscribers);
+        var next, cnt = 0;
+
+        while ((next = ite()) && cnt++ < maxBroadcasts) {
+          next.write('data: ' + data + '\n\n');
+        }
+
+        res.end();
       });
-    });
-  }
+      return;
+    }
 
-  return self;
-}
+    if (req.method === 'GET') {
+      res.setHeader('Content-Type', 'text/event-stream; charset=utf-8');
+      var channel = get(name);
+      server.emit('subscribe', channel.name);
+      channel.subscribers.push(res);
+      eos(res, function () {
+        var i = channel.subscribers.indexOf(res);
+        if (i > -1) channel.subscribers.splice(i, 1);
+        if (!channel.subscribers.length && channel === channels[channel.name])
+          delete channels[channel.name];
+      });
+      if (res.flushHeaders) res.flushHeaders();
+      return;
+    }
+
+    res.statusCode = 404;
+    res.end();
+  }))
+
+  return server;
+};
